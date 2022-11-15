657e4093ed6eeb6efaa05c8d5e586cac6ac05f76
msultanr2057051019 <83525246+MSultan-2057051019@users.noreply.github.com>
2022-11-15 12:41:28 +0700

    forgot password

--- app/Views/customer/forgot_password.php
+++ app/Views/customer/forgot_password.php
@@ -2,50 +2,48 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Mazer Admin Dashboard</title>
-    <link rel="stylesheet" href="/assets/css/main/app.css">
-    <link rel="stylesheet" href="/assets/css/pages/auth.css">
-    <link rel="shortcut icon" href="/assets/images/logo/favicon.svg" type="image/x-icon">
-    <link rel="shortcut icon" href="/assets/images/logo/favicon.png" type="image/png">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Login - Brand</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter&amp;display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins&amp;display=swap">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
 </head>
 
-<body>
-    <div id="auth">
-        
-<div class="row h-100">
-    <div class="col-lg-5 col-12">
-        <div id="auth-left">
-            <div class="auth-logo">
-                <a href="index.html"><img src="/assets/images/logo/logo.svg" alt="Logo"></a>
-            </div>
-            <h1 class="auth-title">Forgot Password</h1>
-            <p class="auth-subtitle mb-5">Input your email and we will send you reset password link.</p>
-
-            <form action="">
-                <div class="form-group position-relative has-icon-left mb-4">
-                    <input type="email" class="form-control form-control-xl" placeholder="Email" required name="email">
-                    <div class="form-control-icon">
-                        <i class="bi bi-envelope"></i>
+<body class="bg-gradient-primary" style="background: linear-gradient(#136c16ee, #136c16f3 55%), url(&quot;https://c4.wallpaperflare.com/wallpaper/971/27/637/beach-bungalow-view-out-in-the-maldives-wallpaper-preview.jpg&quot;), #136c16ee;">
+    <div class="container">
+        <div class="row justify-content-center" style="padding-top: 55px;">
+            <div class="col-md-9 col-lg-6 col-xl-5 col-xxl-5">
+                <div class="card shadow-lg o-hidden border-0 my-5">
+                    <div class="card-body p-0">
+                        <div class="row">
+                            <div class="col-lg-12 col-xl-11 col-xxl-11">
+                                <div class="p-5" style="height: 444.516px;">
+                                    <div class="text-center">
+                                        <h4 class="text-dark mb-4"><i class="fas fa-key"></i><strong>&nbsp; Forgot Password</strong></h4>
+                                    </div>
+                                    <form class="user">
+                                        <div class="mb-3"><input class="form-control form-control-user" type="email" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..." name="email" style="margin-top: 48px;margin-bottom: 45px;"></div>
+                                        <div class="mb-3"></div>
+                                        <div class="mb-3">
+                                            <div class="custom-control custom-checkbox small"></div>
+                                        </div><button class="btn btn-primary d-block btn-user w-100" type="submit">Submit</button>
+                                        <hr>
+                                    </form>
+                                    <div class="text-center"><a class="small" href="/konsumenIndex">Kembali ke Login</a></div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Send</button>
-            </form>
-            <div class="text-center mt-5 text-lg fs-4">
-                <p class='text-gray-600'>Remember your account? <a href="/konsumenIndex/" class="font-bold">Log in</a>.
-                </p>
             </div>
         </div>
     </div>
-    <div class="col-lg-7 d-none d-lg-block">
-        <div id="auth-right">
-
-        </div>
-    </div>
-</div>
-
-    </div>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/bs-init.js"></script>
+    <script src="assets/js/theme.js"></script>
 </body>
 
 </html>
