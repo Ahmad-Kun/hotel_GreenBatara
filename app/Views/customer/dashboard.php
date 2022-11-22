4ec3d1dba15ee28d0b2d345aa6a7bdb00d42af53
M. Thoriq Al Fajri <83461390+thoriqalfajri@users.noreply.github.com>
2022-11-22 21:07:28 +0700

    hiyahiya

--- app/Views/customer/dashboard.php
+++ app/Views/customer/dashboard.php
@@ -11,7 +11,7 @@
                         <div class="row align-items-center no-gutters">
                             <div class="col me-2">
                                 <div class="text-uppercase text-primary fw-bold text-xs mb-1"><span>Earnings (monthly)</span></div>
-                                <div class="text-dark fw-bold h5 mb-0"><span>$40,000</span></div>
+                                <div class="text-dark fw-bold h5 mb-0"><span>Rp 210.000</span></div>
                             </div>
                             <div class="col-auto"><i class="fas fa-calendar fa-2x text-gray-300"></i></div>
                         </div>
@@ -24,7 +24,7 @@
                         <div class="row align-items-center no-gutters">
                             <div class="col me-2">
                                 <div class="text-uppercase text-success fw-bold text-xs mb-1"><span>Earnings (annual)</span></div>
-                                <div class="text-dark fw-bold h5 mb-0"><span>$215,000</span></div>
+                                <div class="text-dark fw-bold h5 mb-0"><span>Rp 210.000</span></div>
                             </div>
                             <div class="col-auto"><i class="fas fa-dollar-sign fa-2x text-gray-300"></i></div>
                         </div>
@@ -36,10 +36,10 @@
                     <div class="card-body">
                         <div class="row align-items-center no-gutters">
                             <div class="col me-2">
-                                <div class="text-uppercase text-info fw-bold text-xs mb-1"><span>Tasks</span></div>
+                                <div class="text-uppercase text-info fw-bold text-xs mb-1"><span>Star</span></div>
                                 <div class="row g-0 align-items-center">
                                     <div class="col-auto">
-                                        <div class="text-dark fw-bold h5 mb-0 me-3"><span>50%</span></div>
+                                        <div class="text-dark fw-bold h5 mb-0 me-3"><span>3 Star</span></div>
                                     </div>
                                     <div class="col">
                                         <div class="progress progress-sm">
@@ -58,11 +58,38 @@
                     <div class="card-body">
                         <div class="row align-items-center no-gutters">
                             <div class="col me-2">
-                                <div class="text-uppercase text-warning fw-bold text-xs mb-1"><span>Pending Requests</span></div>
-                                <div class="text-dark fw-bold h5 mb-0"><span>18</span></div>
+                                <div class="text-uppercase text-warning fw-bold text-xs mb-1"><span>Pending Reservation</span></div>
+                                <div class="text-dark fw-bold h5 mb-0"><span>3</span></div>
                             </div>
                             <div class="col-auto"><i class="fas fa-comments fa-2x text-gray-300"></i></div>
                         </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6 mb-4">
+                <div class="card shadow mb-4">
+                    <div class="card-header py-3">
+                        <p class="text-primary m-0 fw-bold">Earnings Overview</p>
+                    </div>
+                    <div class="card-body">
+                        <div class="chart-area">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 mb-4">
+                <div class="card shadow mb-4">
+                    <div class="card-header py-3">
+                        <p class="text-primary m-0 fw-bold">Feedback</p>
+                    </div>
+                    <div class="card-body">
+                        <h4 class="small font-weight-bold">@Alrizie<span class="float-end">Sangat Baik</span></h4>
+                        <h4 class="small font-weight-bold">@Sultanzz<span class="float-end">Tidur Ternyenyak yang saya alami</span></h4>
+                        <h4 class="small font-weight-bold">@faizz<span class="float-end">Hidup ini cuman sekali jadi lakukan apa yang ingin kamu lakukan</span></h4>
+                        <h4 class="small font-weight-bold">@torique<span class="float-end">True the smoke</span></h4>
                     </div>
                 </div>
             </div>
