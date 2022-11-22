ddfad02540ef62d891bf6cfcd57bb3ceb709b559
Muhammad Faiz <95564115+MuhammadFaiz28@users.noreply.github.com>
2022-11-22 21:27:02 +0700

    upload baru

--- app/Views/customer/profil.php
+++ app/Views/customer/profil.php
@@ -5,8 +5,7 @@
     <div class="row mb-3">
         <div class="col-lg-4">
             <div class="card mb-3">
-                <div class="card-body text-center shadow"><img class="rounded-circle mb-3 mt-4" src="/assets/img/dogs/image2.jpeg" width="190" height="190">
-                    <div class="mb-3"><button class="btn btn-primary btn-sm" type="button">Ganti Foto</button></div>
+                <div class="card-body text-center shadow"><img class="rounded-circle mb-3 mt-4" src="/assets/img/dogs/image2.jpeg" width="190" height="210">
                 </div>
             </div>
         </div>
@@ -45,27 +44,31 @@
                 <div class="col">
                     <div class="card shadow mb-3">
                         <div class="card-header py-3">
-                            <p class="text-primary m-0 fw-bold">Pengaturan User</p>
+                            <p class="text-primary m-0 fw-bold">Profil User</p>
                         </div>
                         <div class="card-body">
-                            <form>
+                            <form action="/editprofil/<?= session()->get('ses_email') ?>">
                                 <div class="row">
                                     <div class="col">
-                                        <div class="mb-3"><label class="form-label" for="username"><strong>Username</strong></label><input class="form-control" type="text" id="username" placeholder="<?= $profile['username']?>" name="username"></div>
+                                        <div class="mb-3"><label class="form-label" for="username"><strong>Username</strong></label><br>
+                                        <?= $profile['username']?></div>
                                     </div>
                                     <div class="col">
-                                        <div class="mb-3"><label class="form-label" for="email"><strong>Email</strong></label><input class="form-control" type="email" id="email" placeholder="<?= $profile['email']?>" name="email"></div>
+                                        <div class="mb-3"><label class="form-label" for="first_name"><strong>Jenis Kelamin</strong></label><br>
+                                        <?= $profile['jk']?></div>
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="col">
-                                        <div class="mb-3"><label class="form-label" for="first_name"><strong>Nama Awal</strong></label><input class="form-control" type="text" id="first_name" placeholder="Root" name="first_name"></div>
+                                        <div class="mb-3"><label class="form-label" for="first_name"><strong>Email</strong></label><br>
+                                        <?= $profile['email']?></div>
                                     </div>
                                     <div class="col">
-                                        <div class="mb-3"><label class="form-label" for="last_name"><strong>Nama Akhir</strong></label><input class="form-control" type="text" id="last_name" placeholder="Amad" name="last_name"></div>
+                                        <div class="mb-3"><label class="form-label" for="last_name"><strong>Deskripsi</strong></label><br>
+                                        <?= $profile['deskripsi']?></div>
                                     </div>
                                 </div>
-                                <div class="mb-3"><button class="btn btn-primary btn-sm" type="submit">Simpan</button></div>
+                                <div class="mb-3"><button class="btn btn-primary btn-sm" type="submit">Edit Profil</button></div>
                             </form>
                         </div>
                     </div>
